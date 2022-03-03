88cf12de9678e1853244f4f12fb558753a63fecd
kaanlab <kaan@live.ru>
2022-03-03 11:21:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RazorFakeSite
+ 
---
+++ RazorFakeSite/Pages/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorFakeSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        [Required]
+        public string Username { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Password { get; set; }
+
+        public string Msg { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Username.Equals("abc") && Password.Equals("123"))
+                {
+                    HttpContext.Session.SetString("username", Username);
+                    return RedirectToPage("Admin");
+                }
+
+                Msg = "Wrong username or password!";
+                return Page();
+            }
+
+            Msg = "Invalid!";
+            return Page();
+
+        }
+    }
+}
